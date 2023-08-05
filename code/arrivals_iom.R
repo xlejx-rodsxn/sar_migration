b7ef02b2f213742aba48c9b36b25b594745138d7
Alejandra Rodríguez Sánchez <56285766+xlejx-rodsxn@users.noreply.github.com>
2023-08-05 16:10:22 +0200

    Update arrivals_iom.R

--- code/arrivals_iom.R
+++ code/arrivals_iom.R
@@ -1,13 +1,13 @@
 #IOM online available arrival data
-'iom_arrivals <- read_excel(path = "C:\\Users\\sanchez\\OneDrive - DeZIM-Institut e.V\\Dokumente\\DEZIM\\SAR project\\data\\IOM monthly arrivals\\IOM_monthly.xlsx")
+'iom_arrivals <- read_excel(path = "IOM_monthly.xlsx")
 iom_arrivals <- iom_arrivals %>% 
   mutate(date = ym(paste0(year,"-",month))) %>% 
   dplyr::select(c(date, land_arrivals, sea_arrivals, total_arrivals))
 '
-IOM_DTM_data1 <- read_excel(path = "C:\\Users\\sanchez\\OneDrive - DeZIM-Institut e.V\\Dokumente\\DEZIM\\SAR project\\data\\DTM_Europe_CMR_Arrivals ITA MLT_2010-2021.xlsx",
+IOM_DTM_data1 <- read_excel(path = "DTM_Europe_CMR_Arrivals ITA MLT_2010-2021.xlsx",
                             sheet = "Nationality")
 
-IOM_DTM_data2 <- read_excel(path = "C:\\Users\\sanchez\\OneDrive - DeZIM-Institut e.V\\Dokumente\\DEZIM\\SAR project\\data\\DTM_Europe_CMR_Arrivals ITA MLT_2010-2021.xlsx",
+IOM_DTM_data2 <- read_excel(path = "DTM_Europe_CMR_Arrivals ITA MLT_2010-2021.xlsx",
                             sheet = "Arrivals")
 IOM_DTM_data2_all <- IOM_DTM_data2 %>% 
   group_by(`Arrival Date`, `Country of<br/> Departure`) %>% 
